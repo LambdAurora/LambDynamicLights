0efc013ab3a402acfd61ae1183c727df60b1aca9
LambdAurora <aurora42lambda@gmail.com>
2020-07-16 16:27:36 +0200

    LambDynamicLights v1.2.2: Fix random crash.

--- src/main/java/me/lambdaurora/lambdynlights/mixin/VideoOptionsScreenMixin.java
+++ src/main/java/me/lambdaurora/lambdynlights/mixin/VideoOptionsScreenMixin.java
@@ -9,13 +9,15 @@
 
 package me.lambdaurora.lambdynlights.mixin;
 
-import me.lambdaurora.lambdynlights.LambDynLights;
+import me.lambdaurora.lambdynlights.gui.SettingsScreen;
 import me.lambdaurora.spruceui.Tooltip;
+import me.lambdaurora.spruceui.option.SpruceSimpleActionOption;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.VideoOptionsScreen;
 import net.minecraft.client.gui.screen.options.GameOptionsScreen;
 import net.minecraft.client.gui.widget.ButtonListWidget;
 import net.minecraft.client.options.GameOptions;
+import net.minecraft.client.options.Option;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
@@ -30,15 +32,23 @@ public class VideoOptionsScreenMixin extends GameOptionsScreen
     @Shadow
     private ButtonListWidget list;
 
+    private Option lambdynlights_option;
+
     public VideoOptionsScreenMixin(Screen parent, GameOptions gameOptions, Text title)
     {
         super(parent, gameOptions, title);
     }
 
+    @Inject(method = "<init>", at = @At("TAIL"))
+    private void onConstruct(Screen parent, GameOptions gameOptions, CallbackInfo ci)
+    {
+        this.lambdynlights_option = new SpruceSimpleActionOption("lambdynlights.menu.title", btn -> this.client.openScreen(new SettingsScreen(this)));
+    }
+
     @Inject(method = "init", at = @At("TAIL"))
     private void onInit(CallbackInfo ci)
     {
-        this.list.addSingleOptionEntry(LambDynLights.get().config.dynamicLightsModeOption);
+        this.list.addSingleOptionEntry(this.lambdynlights_option);
     }
 
     @Inject(method = "render", at = @At("TAIL"))
