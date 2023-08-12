92708b5fc5d43b36bde196509d49e31f637cdee1
Naz <bluspringindohq@gmail.com>
2023-08-12 17:18:14 +0800

    Ensure Sodium mixin doesn't crash in production

--- src/main/java/dev/lambdaurora/lambdynlights/mixin/sodium/LightDataAccessMixin.java
+++ src/main/java/dev/lambdaurora/lambdynlights/mixin/sodium/LightDataAccessMixin.java
@@ -11,13 +11,13 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Pseudo
-@Mixin(targets = "me.jellysquid.mods.sodium.client.model.light.data.LightDataAccess")
+@Mixin(targets = "me.jellysquid.mods.sodium.client.model.light.data.LightDataAccess", remap = false)
 public class LightDataAccessMixin {
     // In Sodium 0.5, LightDataAccess has replaced the Vanilla LightmapTextureManager,
     // and no longer uses WorldRenderer#getLightmapCoordinates.
     // Unfortunately, this breaks LambDynLights, and as a result is forced to mixin to Sodium.
     @Dynamic
-    @Redirect(method = "compute", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/BlockRenderView;getLightLevel(Lnet/minecraft/world/LightType;Lnet/minecraft/util/math/BlockPos;)I", ordinal = 0), remap = false)
+    @Redirect(method = "compute", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/BlockRenderView;getLightLevel(Lnet/minecraft/world/LightType;Lnet/minecraft/util/math/BlockPos;)I", ordinal = 0, remap = true), remap = false)
     private int lambdynlights$modifyBlockLight(BlockRenderView world, LightType type, BlockPos pos, int x, int y, int z) {
         int original = world.getLightLevel(type, pos);
 
