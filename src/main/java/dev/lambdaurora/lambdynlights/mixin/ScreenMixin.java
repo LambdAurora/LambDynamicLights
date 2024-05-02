c6493ab60f77214697695ec0bd3991417fc4408e
lliioollcn <lliiooll@126.com>
2024-05-02 19:18:43 +0800

    bad fix

---
+++ src/main/java/dev/lambdaurora/lambdynlights/mixin/ScreenMixin.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright © 2020 LambdAurora <email@lambdaurora.dev>
+ *
+ * This file is part of LambDynamicLights.
+ *
+ * Licensed under the MIT license. For more information,
+ * see the LICENSE file.
+ */
+
+
+package dev.lambdaurora.lambdynlights.mixin;
+
+import dev.lambdaurora.spruceui.Tooltip;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.option.VideoOptionsScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Screen.class)
+public class ScreenMixin {
+
+    @Inject(method = "render",
+            at = @At(value = "TAIL"))
+    private void onRender(GuiGraphics graphics, int mouseX, int mouseY, float delta, CallbackInfo ci) {
+        if (((Screen) (Object) this) instanceof VideoOptionsScreen) {
+            Tooltip.renderAll(graphics);
+        }
+    }
+}
--- src/main/java/dev/lambdaurora/lambdynlights/mixin/VideoOptionsScreenMixin.java
+++ src/main/java/dev/lambdaurora/lambdynlights/mixin/VideoOptionsScreenMixin.java
@@ -27,35 +27,32 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(VideoOptionsScreen.class)
 public class VideoOptionsScreenMixin extends GameOptionsScreen {
-	@Unique
-	private Option<?> lambdynlights$option;
+    @Unique
+    private Option<?> lambdynlights$option;
 
-	public VideoOptionsScreenMixin(Screen parent, GameOptions gameOptions, Text title) {
-		super(parent, gameOptions, title);
-	}
+    public VideoOptionsScreenMixin(Screen parent, GameOptions gameOptions, Text title) {
+        super(parent, gameOptions, title);
+    }
 
-	@Inject(method = "<init>", at = @At("TAIL"))
-	private void onConstruct(Screen parent, GameOptions gameOptions, CallbackInfo ci) {
-		this.lambdynlights$option = DynamicLightsOptionsOption.getOption(this);
-	}
+    @Inject(method = "<init>", at = @At("TAIL"))
+    private void onConstruct(Screen parent, GameOptions gameOptions, CallbackInfo ci) {
+        this.lambdynlights$option = DynamicLightsOptionsOption.getOption(this);
+    }
 
-	@ModifyArg(
-			method = "init",
-			at = @At(
-					value = "INVOKE",
-					target = "Lnet/minecraft/client/gui/widget/list/ButtonListWidget;addEntries([Lnet/minecraft/client/option/Option;)V"
-			),
-			index = 0
-	)
-	private Option<?>[] addOptionButton(Option<?>[] old) {
-		var options = new Option<?>[old.length + 1];
-		System.arraycopy(old, 0, options, 0, old.length);
-		options[options.length - 1] = this.lambdynlights$option;
-		return options;
-	}
+    @ModifyArg(
+            method = "init",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/gui/widget/list/ButtonListWidget;addEntries([Lnet/minecraft/client/option/Option;)V"
+            ),
+            index = 0
+    )
+    private Option<?>[] addOptionButton(Option<?>[] old) {
+        var options = new Option<?>[old.length + 1];
+        System.arraycopy(old, 0, options, 0, old.length);
+        options[options.length - 1] = this.lambdynlights$option;
+        return options;
+    }
 
-	@Inject(method = "render", at = @At("TAIL"))
-	private void onRender(GuiGraphics graphics, int mouseX, int mouseY, float delta, CallbackInfo ci) {
-		Tooltip.renderAll(graphics);
-	}
+
 }
