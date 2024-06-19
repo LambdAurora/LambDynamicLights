ddb50b3d7477eced2c9aaa3bda58a7bbb5ce076d
Naz <bluspringindohq@gmail.com>
2024-06-20 07:13:32 +0800

    Use ModifyExpressionValue instead of WrapOperation

--- src/main/java/dev/lambdaurora/lambdynlights/mixin/indium/TerrainRenderContextMixin.java
+++ src/main/java/dev/lambdaurora/lambdynlights/mixin/indium/TerrainRenderContextMixin.java
@@ -9,10 +9,8 @@
 
 package dev.lambdaurora.lambdynlights.mixin.indium;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import dev.lambdaurora.lambdynlights.LambDynLights;
-import link.infra.indium.renderer.mesh.MutableQuadViewImpl;
 import link.infra.indium.renderer.render.AbstractBlockRenderContext;
 import link.infra.indium.renderer.render.TerrainRenderContext;
 import org.spongepowered.asm.mixin.Dynamic;
@@ -24,8 +22,8 @@ import org.spongepowered.asm.mixin.injection.At;
 @Mixin(value = TerrainRenderContext.class, remap = false)
 public abstract class TerrainRenderContextMixin extends AbstractBlockRenderContext {
 	@Dynamic
-	@WrapOperation(method = "bufferQuad", at = @At(value = "INVOKE", target = "Llink/infra/indium/renderer/mesh/MutableQuadViewImpl;lightmap(I)I"), require = 0)
-	private int lambdynlights$getLightmap(MutableQuadViewImpl instance, int i, Operation<Integer> original) {
-		return LambDynLights.get().getLightmapWithDynamicLight(this.blockInfo.blockPos, original.call(instance, i));
+	@ModifyExpressionValue(method = "bufferQuad", at = @At(value = "INVOKE", target = "Llink/infra/indium/renderer/mesh/MutableQuadViewImpl;lightmap(I)I"), require = 0)
+	private int lambdynlights$getLightmap(int original) {
+		return LambDynLights.get().getLightmapWithDynamicLight(this.blockInfo.blockPos, original);
 	}
 }
