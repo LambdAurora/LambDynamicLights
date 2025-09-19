b7a136f34ab12242694f93c16c17b8b1c2ce914f
LambdAurora <email@lambdaurora.dev>
2025-09-19 08:25:40 +0200

    Revert "Remove useless Indigo mixin."
    This reverts commit 584bb2b37f409e07cdb62edb4b1e923adf7070a3.

--- build.gradle.kts
+++ build.gradle.kts
@@ -68,6 +68,7 @@ dependencies {
 
 	modImplementation(libs.fabric.loader)
 	fabricApiModules.forEach { modImplementation(it) }
+	//modRuntimeOnly(fabricApi.module("fabric-renderer-indigo", libs.versions.fabric.api.get()))
 	modImplementation(libs.yumi.mc.foundation)
 	include(libs.yumi.mc.foundation)
 
