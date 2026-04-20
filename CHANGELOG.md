34702dd7c1f930d74653526cc4ca9bf431ea6dfb
Layla <email@lambdaurora.dev>
2026-04-20 23:15:53 +0200

    Improve compatibility by using chainable injections.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -559,6 +559,17 @@
 - Updated Ukrainian translations ([#334](https://github.com/LambdAurora/LambDynamicLights/pull/334)).
 - Updated Argentine Spanish translations ([#339](https://github.com/LambdAurora/LambDynamicLights/pull/339)).
 
+### 4.8.8
+
+- Improved compatibility by using chainable injections.
+- Backported some changes:
+  - Added Czech translations ([#357](https://github.com/LambdAurora/LambDynamicLights/pull/357)).
+  - Updated Ukrainian translations ([#355](https://github.com/LambdAurora/LambDynamicLights/pull/355)).
+  - Updated Traditional Chinese translations ([#363](https://github.com/LambdAurora/LambDynamicLights/pull/363), [#365](https://github.com/LambdAurora/LambDynamicLights/pull/365)).
+  - Updated Simplified Chinese translations ([#364](https://github.com/LambdAurora/LambDynamicLights/pull/364), [#365](https://github.com/LambdAurora/LambDynamicLights/pull/365)).
+  - Updated Japanese translations ([#365](https://github.com/LambdAurora/LambDynamicLights/pull/365)).
+  - Fixed Simplified Chinese translations ([#368](https://github.com/LambdAurora/LambDynamicLights/issues/368)).
+
 ## 4.9.0
 
 - Updated to Minecraft 1.21.11.
@@ -585,6 +596,10 @@
 - Readded Trinkets compatibility with Trinkets Updated ([#367](https://github.com/LambdAurora/LambDynamicLights/issues/367)).
 - Fixed Simplified Chinese translations ([#368](https://github.com/LambdAurora/LambDynamicLights/issues/368)).
 - Updated [Yumi Minecraft Libraries: Foundation].
+
+### 4.10.2
+
+- Improved compatibility by using chainable injections.
 
 [SpruceUI]: https://github.com/LambdAurora/SpruceUI "SpruceUI page"
 [pridelib]: https://github.com/Queerbric/pridelib "Pridelib page"
--- src/main/java/dev/lambdaurora/lambdynlights/mixin/BrightnessGetterMixin.java
+++ src/main/java/dev/lambdaurora/lambdynlights/mixin/BrightnessGetterMixin.java
@@ -9,14 +9,13 @@
 
 package dev.lambdaurora.lambdynlights.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import dev.lambdaurora.lambdynlights.LambDynLights;
 import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.BlockAndLightGetter;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 /**
  * Injects the dynamic lighting into the default brightness getter.
@@ -24,24 +23,25 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
  * Injecting into the default brightness getter allows to benefit from brightness caching in the Vanilla renderer.
  *
  * @author LambdAurora
- * @version 4.10.0
+ * @version 4.10.2
  * @since 4.2.3
  */
 @Mixin(value = LevelRenderer.BrightnessGetter.class, priority = 900)
 public interface BrightnessGetterMixin {
-	@Inject(
+	@ModifyReturnValue(
 			method = "lambda$static$0",
-			at = @At("TAIL"),
-			cancellable = true,
+			at = @At("RETURN"),
 			remap = false,
 			allow = 1,
 			require = 1
 	)
-	private static void onGetLightmapCoordinates(
-			BlockAndLightGetter level, BlockPos pos,
-			CallbackInfoReturnable<Integer> cir
+	private static int onGetLightmapCoordinates(
+			int original,
+			BlockAndLightGetter level, BlockPos pos
 	) {
 		if (!level.getBlockState(pos).isSolidRender() && LambDynLights.get().config.getDynamicLightsMode().isEnabled())
-			cir.setReturnValue(LambDynLights.get().getLightmapWithDynamicLight(level, pos, cir.getReturnValue()));
+			return LambDynLights.get().getLightmapWithDynamicLight(level, pos, original);
+
+		return original;
 	}
 }
